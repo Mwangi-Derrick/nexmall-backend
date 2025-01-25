d016f12fe24a66da97bfb3dfb70e524c39c5d4ee
Mwangi-Derrick <mwangiderrick640@gmail.com>
2025-01-25 11:49:05 +0300

    added the auth middleware to the route handlers

--- routes/orders.js
+++ routes/orders.js
@@ -1,8 +1,9 @@
 const { createOrder, sendStkPush } = require("../controllers/order.controller");
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
 
-router.post("/", createOrder);
-router.post("/pay", sendStkPush);
+router.post("/",auth, createOrder);
+router.post("/pay",auth, sendStkPush);
 
 module.exports = router
