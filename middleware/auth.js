2242774777dec37be3c2878b42814d1641715221
Mwangi-Derrick <mwangiderrick640@gmail.com>
2025-01-25 11:41:22 +0300

    created middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,9 @@
 const jwt = require("jsonwebtoken");
-const config = require("config");
 function auth(req, res, next) {
     const token = req.header("x-auth-token")
     if (!token) res.status(401).send("Access is denied .Token is required.");
     try {
-        const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+        const decoded = jwt.verify(token,process.env.JWT_PRIVATE_KEY);
         req.user = decoded;
         next();
     }
