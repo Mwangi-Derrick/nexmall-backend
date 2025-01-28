3157a9b1d32a1064600d52303b734ffe6c14837c
Mwangi-Derrick <mwangiderrick640@gmail.com>
2025-01-28 13:35:21 +0300

    added send stk push and handlempesa callback route handlers

--- routes/orders.js
+++ routes/orders.js
@@ -1,9 +1,11 @@
-const { createOrder, sendStkPush } = require("../controllers/order.controller");
+const { createOrder,handleMpesaCallback, sendStkPush } = require("../controllers/order.controller");
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
+const { getAccessToken } = require("../middleware/accessToken");
 
 router.post("/",auth, createOrder);
-router.post("/pay",auth, sendStkPush);
+router.post("/pay", auth,getAccessToken, sendStkPush);
+router.post("/callback", handleMpesaCallback);
 
 module.exports = router
